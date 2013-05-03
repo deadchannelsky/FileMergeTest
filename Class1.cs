e31581d951a69dc56044dd9c91d1db2a2a902ac9
deadchannelsky <deadchannel@mailinator.com>
2013-05-03 16:58:12 -0400

    commit
    commit

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileMergeTest
+{
+    class StringEqualityComparer : IEqualityComparer<string>
+    {
+        // Lower-case your strings for a case insensitive compare.
+        public bool Equals(string s1, string s2)
+        {
+            if (s1.ToLower().Equals(s2.ToLower()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #region IEqualityComparer<string> Members
+        public int GetHashCode(string s)
+        {
+            return s.GetHashCode();
+        }
+
+        #endregion
+    }
+}
