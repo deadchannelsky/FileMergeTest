aa33bcfafcef4fb4f9fde62460b15932ef33920f
deadchannelsky <deadchannel@mailinator.com>
2013-05-03 17:02:20 -0400

    dd
    dd

---
+++ getfile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+
+
+namespace FileMergeTest
+{
+    class getfile
+    {
+        string awsKey = "AKIAIMQI3XVCBELZLYQA";
+        string awsSecret = "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";
+        public void S3put()
+        {
+            string toWhichBucketName = "kspmerge";
+            var client = Amazon.AWSClientFactory.CreateAmazonS3Client(awsKey, awsSecret);
+            PutObjectRequest request = new PutObjectRequest();
+            request.WithFilePath( Environment.CurrentDirectory + "\\merged\\persistent.sfs");
+            request.WithBucketName(toWhichBucketName);
+            client.PutObject(request);
+        }
+
+        public void S3get()
+        {
+            string key = "persistent.sfs";
+            string bucketname = "kspmerge";
+
+            var client = Amazon.AWSClientFactory.CreateAmazonS3Client(awsKey, awsSecret);
+            GetObjectRequest request = new GetObjectRequest();
+            request.WithBucketName(bucketname);
+            request.WithKey(key);
+            GetObjectResponse response = client.GetObject(request);
+            response.WriteResponseStreamToFile(Environment.CurrentDirectory +"\\tmp\\persistent.sfs");
+        }
+
+       
+    }
+}
