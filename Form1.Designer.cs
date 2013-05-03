061de97534b80dc13212a5f80d742305a1361bde
deadchannelsky <deadchannel@mailinator.com>
2013-05-03 17:00:23 -0400

    comm it
    it

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace FileMergeTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.btnGetRemotePersistent = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(157, 365);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(143, 31);
+            this.btnMerge.TabIndex = 0;
+            this.btnMerge.Text = "Merge";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Visible = false;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // btnGetRemotePersistent
+            // 
+            this.btnGetRemotePersistent.Location = new System.Drawing.Point(58, 148);
+            this.btnGetRemotePersistent.Name = "btnGetRemotePersistent";
+            this.btnGetRemotePersistent.Size = new System.Drawing.Size(379, 23);
+            this.btnGetRemotePersistent.TabIndex = 1;
+            this.btnGetRemotePersistent.Text = "Get CloudJeb Universe";
+            this.btnGetRemotePersistent.UseVisualStyleBackColor = true;
+            this.btnGetRemotePersistent.Click += new System.EventHandler(this.btnGetRemotePersistent_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "This is the pre-release version of CloudJeb. All S3 variables are hard coded.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(320, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Please use the Beta or 1.0 release to use your own S3 credentials.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(453, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Get Remote Universe pulls the persistent.sfs file from S3 and merges it with your" +
+                " selected save.";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(60, 215);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(376, 27);
+            this.btnUpload.TabIndex = 8;
+            this.btnUpload.Text = "Upload Local Universe to CloudJeb Universe";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 457);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGetRemotePersistent);
+            this.Controls.Add(this.btnMerge);
+            this.Name = "Form1";
+            this.Text = "CloudJeb";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Button btnGetRemotePersistent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnUpload;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace FileMergeTest
+{
+    public partial class Form1 : Form
+    {
+        string localPersistentPath = null;
+        string workingDir = Environment.CurrentDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            var localUniverse = File.ReadLines(localPersistentPath);
+            var externalUniverse = File.ReadAllLines(workingDir + "\\tmp\\persistent.sfs");
+            var onlyInFileA = localUniverse.Except(externalUniverse);
+            File.WriteAllLines("\\tmp\\merged.sfs", externalUniverse.Concat(onlyInFileA));
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {   
+            
+            MessageBox.Show(new Form1() { TopMost = true }, "Please select the KSP Saves folder that is or will be your local CloudJeb universe.");
+            Directory.CreateDirectory(workingDir + "\\tmp");
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            localPersistentPath = fbd.SelectedPath + "\\persistent.sfs";
+            MessageBox.Show("You have selected " + localPersistentPath + " as your CloudJeb universe. Click OK to continue.");
+            
+        }
+
+        private void btnGetRemotePersistent_Click(object sender, EventArgs e)
+        {
+           //download remote universe           
+
+            getfile gf1 = new getfile();
+            gf1.S3get();
+
+            
+        //merge remote universe and local universe
+
+            var localUniverse = File.ReadLines(localPersistentPath);
+            var externalUniverse = File.ReadAllLines(workingDir + "\\tmp\\persistent.sfs");
+            var onlyInFileA = localUniverse.Except(externalUniverse);
+            File.WriteAllLines(workingDir + "\\tmp\\merged.sfs", externalUniverse.Concat(onlyInFileA));
+            File.Copy(workingDir + "\\tmp\\merged.sfs", localPersistentPath, true);
+            MessageBox.Show(new Form1() { TopMost = true }, "Your local universe is now sync'd with the CloudJeb universe.");
+             
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+          //download remote universe. Incase someone has updated it since you downloaded it. 
+            getfile gf1 = new getfile();
+            gf1.S3get();
+          //merge local and newly downloaded remote. 
+
+            var localUniverse = File.ReadLines(localPersistentPath);
+            var externalUniverse = File.ReadAllLines(workingDir + "\\tmp\\persistent.sfs");
+            var onlyInFileA = localUniverse.Except(externalUniverse);
+            //make dir
+            Directory.CreateDirectory(workingDir + "\\merged");
+            File.WriteAllLines(workingDir + "\\merged\\persistent.sfs", externalUniverse.Concat(onlyInFileA));
+
+            //At thispoint persistent.sfs in the merged folder is the remote universe plus local play session time. Now upload persistent.sfs to ftp.
+            getfile gf2 = new getfile();
+            gf2.S3put();
+            
+            
+            MessageBox.Show("Merged and uploaded your universe with the CloudJeb Universe.");
+
+            
+
+        }
+
+       
+    }
+}
